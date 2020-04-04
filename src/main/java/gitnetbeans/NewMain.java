06c613f7655c8e6064a87ecd482ddebb489cfaab
Jose Doval <dovaldam108@gmail.com>
2020-04-04 15:21:58 +0200

    se modifica el main para crear un error

--- src/main/java/gitnetbeans/NewMain.java
+++ src/main/java/gitnetbeans/NewMain.java
@@ -3,7 +3,7 @@ package gitnetbeans;
 public class NewMain {
 
     public static void main(String[] args) {
-        System.out.println(Sumador.sumar (15f,4f));
+        System.out.println(Sumador.sumar (16f,4fo));
         
     }
 }
